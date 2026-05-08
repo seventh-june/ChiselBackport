37adac4f3d37577c6b26469e7307d2686ddeba02
June Awesome <junespaldingg@gmail.com>
2026-05-08 15:56:14 -0400

    gemz

--- src/main/java/team/chisel/init/ChiselBlocks.java
+++ src/main/java/team/chisel/init/ChiselBlocks.java
@@ -197,6 +197,7 @@ public final class ChiselBlocks {
     public static final BlockCarvable iridium = null;
     public static final BlockCarvable osmium = null;
     public static final BlockCarvable coral = null;
+    public static final BlockCarvable coral2 = null;
     public static final BlockCarvable mud = null;
     public static final BlockCarvable albumblock = null;
     public static final BlockCarvable albumblock2 = null;
@@ -206,6 +207,7 @@ public final class ChiselBlocks {
     public static final BlockCarvable road = null;
     public static final BlockCarvable porcelain = null;
     public static final BlockCarvable alabaster = null;
+
 
     private ChiselBlocks() {}
 }
